4e12dfa6a47df65ad13d556001fb453516c53a16
atoha511 <anton.palekhin@gmail.com>
2021-09-28 17:25:55 +0700

    Update QueryBuilder.php

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -1,62 +1,72 @@
 <?php
 
+namespace App;
+
+use Aura\SqlQuery\QueryFactory;
+use PDO;
+
 
 class QueryBuilder
 {
     protected $pdo;
+    protected $queryFactory;
 
-    public function __construct($pdo)
+    public function __construct()
     {
-        $this->pdo = $pdo;
+        $this->pdo = new PDO('mysql:host=localhost;dbname=lesson', 'lesson', '12345');
+        $this->queryFactory = new QueryFactory("mysql");
     }
 
     public function getAll($table)
     {
-        {
-            $sql = "SELECT * FROM $table";
-            $statement = $this->pdo->prepare($sql);
-            $statement->execute();
-            return $statement->fetchAll(PDO::FETCH_ASSOC);
-        }
+        $select = $this->queryFactory->newSelect();
+        $select->cols(['*'])
+            ->from($table);
+        $statement = $this->pdo->prepare($select->getStatement());
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
+
     public function getOne($table, $id)
     {
-        $sql = "SELECT * FROM $table WHERE id = :id";
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute(["id" => $id]);
-        return $statement->fetch(PDO::FETCH_ASSOC);
+        $select = $this->queryFactory->newSelect();
+        $select->cols(['*'])
+            ->from($table)
+            ->where("id = :id")
+            ->bindValue('id', $id);
+        $statement = $this->pdo->prepare($select->getStatement());
+        $statement->execute($select->getBindValues());
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-
-    public function create($table, $data)
+    public function insert($table, $data)
     {
-        $keys = implode(',', array_keys($data));
-        $tags = ':' . implode(', :', array_keys($data));
-        $sql = "INSERT INTO {$table} ({$keys}) VALUES ({$tags})";
-        $statement = $this->pdo->prepare($sql);
+        $insert = $this->queryFactory->newInsert();
+        $insert->cols($data)
+            ->into($table);
+        $statement = $this->pdo->prepare($insert->getStatement());
         $statement->execute($data);
     }
 
     public function update($table, $data, $id)
     {
-        $keys = array_keys($data);
-        $string = '';
-        foreach ($keys as $key){
-            $string .= $key . "=:" . $key . ',';
-        }
-        $keys = rtrim($string, ',');
-        $data['id'] = $id;
-
-        $sql = "UPDATE {$table} SET {$keys} WHERE id=:id";
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute($data);
+        $update = $this->queryFactory->newUpdate();
+        $update->cols($data)
+            ->table($table)
+            ->where('id = :id')
+            ->bindValue('id', $id);
+        $statement = $this->pdo->prepare($update->getStatement());
+        $statement->execute($update->getBindValues());
     }
 
     public function delete($table, $id)
     {
-        $sql = "DELETE FROM {$table} WHERE id = :id";
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute(["id" => $id]);
+        $delete = $this->queryFactory->newDelete();
+        $delete->from($table)
+            ->where('id = :id')
+            ->bindValue('id', $id);
+        $statement = $this->pdo->prepare($delete->getStatement());
+        $statement->execute($delete->getBindValues());
     }
 
 }
